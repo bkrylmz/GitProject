0719a236a4a535140fde4201632265da020000b5
Bekir <bkrylmz981@gmail.com>
2022-12-08 09:54:28 +0300

    added more hello feature

--- Hello.java
+++ Hello.java
@@ -4,6 +4,8 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("Created git repository");
         System.out.println("Made few commit");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
 
 
 
